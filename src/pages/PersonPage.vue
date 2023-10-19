58a8cc0c6ef9acc996eefe5844cb5cb26368aaea
id0ntcareatall <kotvalerakot@mail.ru>
2023-10-19 19:17:46 +0300

    some basic logic for PersonPage was added

--- src/pages/PersonPage.vue
+++ src/pages/PersonPage.vue
@@ -1,8 +1,23 @@
 <template>
-  <div></div>
+  <PeopleTable
+      :peopleList="props.peopleList"
+      @changeFavoritesList="handleChangeFavoritesList"
+  />
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
+import { defineEmits, defineProps, PropType } from "vue";
+import PeopleTable from "../components/PeopleTable.vue";
+import type { Person } from "../../types";
+
+const emit = defineEmits(["changeFavoritesList"]);
+const props = defineProps({
+  peopleList: Array as PropType<Person[]>,
+});
+
+const handleChangeFavoritesList = (selectedPersonName) => {
+  emit("changeFavoritesList", selectedPersonName);
+};
 </script>
 
 <style scoped>
