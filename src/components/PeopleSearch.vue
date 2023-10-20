e713687f1506323a5d83d630812b1fab893c0e72
id0ntcareatall <kotvalerakot@mail.ru>
2023-10-20 19:04:33 +0300

    logic of changing route and debouncing search was added

--- src/components/PeopleSearch.vue
+++ src/components/PeopleSearch.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="search">
     <img src="../assets/search.png"/>
-    <input type="text" v-model="searchQuery" @input="search" placeholder="Search">
+    <input type="text" v-model.trim="searchQuery" @input="debouncedSearch" placeholder="Search">
     <ul class="result" v-if="searchResult.length !== 0">
-      <li v-for="person in searchResult" :key="person.name">
+      <li v-for="person in searchResult" :key="person.name" @click="showPerson(person)">
         name: {{ person.name }}
       </li>
     </ul>
@@ -14,6 +14,8 @@
 <script lang="ts" setup>
 import { PropType, ref } from "vue";
 import type { Person } from "../../types";
+import { debounce } from "lodash";
+import router from "../router";
 
 const props = defineProps({
   peopleList: Array as PropType<Person[]>,
@@ -26,15 +28,15 @@ const showNotFound = ref<boolean>(false);
 const search = async () => {
   showNotFound.value = false;
   try {
-    if (searchQuery.value === "") {
-      searchResult.value = [];
-      return;
-    }
     const response = await fetch("https://swapi.dev/api/people/?search=" + searchQuery.value);
     if (!response.ok) {
       throw new Error("Failed to fetch data");
     }
 
+    if (searchQuery.value === "") {
+      searchResult.value = [];
+      return;
+    }
     const data = await response.json();
     if (data.results.length === 0) {
       showNotFound.value = true;
@@ -46,7 +48,16 @@ const search = async () => {
   }
 };
 
-const arr = [];
+const debouncedSearch = debounce(search, 300);
+
+const showPerson = (person) => {
+  const splitUrl = person.url.split("/");
+  const personId = splitUrl[splitUrl.length - 2];
+  router.push({
+    path: `/people/${personId}`, params: { data: person },
+  });
+};
+
 </script>
 
 <style scoped>
